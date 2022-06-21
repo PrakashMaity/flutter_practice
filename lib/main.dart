2d17be597cf5be6523a65ea013745f18bb721bfd
PrakashMaity <prakashmaity62@gmail.com>
2022-06-21 23:47:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:newapp/screen/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'new app',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('new app')),
+        body: const Home(),
+      ),
+    );
+  }
+}
